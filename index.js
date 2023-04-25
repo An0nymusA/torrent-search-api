f986ebe319c33a91593314b8bf6982311b07907f
Adam Zvěřina <adam.zverina@gmail.com>
2023-04-26 00:54:38 +0200

    Fixed providers on electron

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const path = require('path');
 const createApi = require('./createApi');
 
-module.exports = createApi(path.join(__dirname, './lib/providers'));
+const providers = [
+  require('./lib/providers/1337x.js'),
+  require('./lib/providers/eztv.json'),
+  require('./lib/providers/kickasstorrents.json'),
+  require('./lib/providers/limetorrents.json'),
+  require('./lib/providers/rarbg.js'),
+  require('./lib/providers/yts.js')
+];
+
+module.exports = createApi(...providers);
