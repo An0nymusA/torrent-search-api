c0b8b3c14e50199bb206510a865df37198fa7696
Adam Zvěřina <adam.zverina@gmail.com>
2023-04-26 09:59:37 +0200

    Another bugfix

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const createApi = require('./createApi');
 const providers = [
   require('./lib/providers/1337x.js'),
   require('./lib/providers/eztv.json'),
-  require('./lib/providers/kickasstorrents.json'),
   require('./lib/providers/limetorrents.json'),
-  require('./lib/providers/rarbg.js'),
-  require('./lib/providers/yts.js')
+  // require('./lib/providers/rarbg.js'),
+  require('./lib/providers/yts.js'),
+  require('./lib/providers/torrent9.js')
 ];
 
 module.exports = createApi(...providers);
